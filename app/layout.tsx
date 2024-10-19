6697bd85828893b805ab9c891cd99fbf8da5d90c
mihalacheD <danutza_dima88@yahoo.com>
2024-10-19 16:09:15 +0300

    add a markdown editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
-import { ThemePanel } from "@radix-ui/themes";
+
 
 
 
