fc954305540a46b5944f0c1f1fb7c995cbdc58dd
mihalacheD <danutza_dima88@yahoo.com>
2024-10-23 17:19:52 +0300

    refactor: organize import statements

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,18 @@
 'use client';
-import { Callout, TextField, Button } from '@radix-ui/themes'
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
+import { createIssueSchema } from '@/app/validationSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import axios from 'axios';
+import "easymde/dist/easymde.min.css";
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'),
                                 { ssr: false } );
-import { useForm, Controller, set } from 'react-hook-form';
-import { useRouter } from 'next/navigation';
-import axios from 'axios'
-import "easymde/dist/easymde.min.css";
-import React from 'react'
-import { useState } from 'react';
-import { zodResolver } from '@hookform/resolvers/zod'
-import { createIssueSchema } from '@/app/validationSchema';
-import { z } from 'zod';
-import Spinner from '@/app/components/Spinner';
-import ErrorMessage from '@/app/components/ErrorMessage';
 
 
 
