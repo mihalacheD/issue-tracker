2018e78594e93adaf566427b60ef47bea228bad9
mihalacheD <danutza_dima88@yahoo.com>
2024-10-24 14:53:11 +0300

    add the edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { Card, Flex, Grid, Heading, Text, Box, Button } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import ReactMarkdown from 'react-markdown'
+import { Pencil2Icon } from "@radix-ui/react-icons"
+import Link from 'next/link'
 
 
 interface Props {
@@ -19,7 +21,9 @@ const IssueDetailPage = async ({ params }: Props) => {
 
 
   return (
-    <div>
+    <Grid columns={{ initial: '1', md: '2'}} gap='5'>
+
+      <Box>
       <Heading>{issue.title}</Heading>
       <Flex gap='5' my='2'>
       <IssueStatusBadge status={issue.status}/>
@@ -28,7 +32,15 @@ const IssueDetailPage = async ({ params }: Props) => {
       <Card className='prose' mt='4'>
       <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
-    </div>
+      </Box>
+
+      <Box>
+        <Button>
+          <Pencil2Icon/>
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   )
 }
 
