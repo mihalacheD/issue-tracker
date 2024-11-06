886d1ab432a398781759fee48fa4ce8262a3f776
mihalacheD <danutza_dima88@yahoo.com>
2024-11-06 15:54:33 +0200

    populate the assignee select component

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -34,7 +34,7 @@ const IssueDetailPage = async ({ params }: Props) => {
        {session && (
       <Box>
         <Flex direction='column' gap='4'>
-          <AssigneeSelect/>
+        <AssigneeSelect/>
         <EditIssueButton issueId={issue.id}/>
         <DeleteIssueButton issueId={issue.id}/>
         </Flex>
