2d2c4168071fe142982298f04048f1199cc2e6ec
mihalacheD <danutza_dima88@yahoo.com>
2024-11-15 14:16:45 +0200

    reconect with the database

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -59,8 +59,8 @@ const IssueForm = ({ issue }: Props) => {
         className='max-w-xl space-y-3'
         onSubmit={onSubmit}>
 
-      <TextField.Root defaultValue={issue?.title} placeholder='Title' { ...register('title')} >
-        <TextField.Slot />
+      <TextField.Root >
+        <TextField.Input  defaultValue={issue?.title} placeholder='Title' { ...register('title')}/>
       </TextField.Root>
       <ErrorMessage>{ errors.title?.message}</ErrorMessage>
       <Controller
